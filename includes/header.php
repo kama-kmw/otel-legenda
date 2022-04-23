13fd8bb420f6359c7832c63252048b61742948ed
Surha <surha_dag@mail.ru>
2022-04-23 04:05:03 +0300

    правка ссылок в хедере заменил ссылки с html на php

--- includes/header.php
+++ includes/header.php
@@ -6,26 +6,26 @@
         <div class="ml-auto d-flex flex-row align-items-center justify-content-start">
             <nav class="main_nav">
                 <ul class="d-flex flex-row align-items-start justify-content-start">
-                    <li><a href="index.html">Главная</a></li>
-                    <li><a href="about.html">О нас</a></li>
+                    <li><a href="index.php">Главная</a></li>
+                    <li><a href="about.php">О нас</a></li>
                     <li><a href="#">Номера</a>
                         <ul class="sub-menu">
                             <li>
-                                <a href="booking_2.html" class="main-menu__link">2-х местный</a>
+                                <a href="booking_2.php" class="main-menu__link">2-х местный</a>
                             </li>
                             <li>
-                                <a href="booking_3.html" class="main-menu__link">3-х местный</a>
+                                <a href="booking_3.php" class="main-menu__link">3-х местный</a>
                             </li>
                             <li>
-                                <a href="booking_4.html" class="main-menu__link">4-х местный</a>
+                                <a href="booking_4.php" class="main-menu__link">4-х местный</a>
                             </li>
                         </ul>
                     </li>
-                    <li><a href="blog.html">Галерея</a></li>
-                    <li class="active"><a href="contact.html">Контакты</a></li>
+                    <li><a href="blog.php">Галерея</a></li>
+                    <li class="active"><a href="contact.php">Контакты</a></li>
                 </ul>
             </nav>
-            <!-- <div class="book_button"><a href="booking.html">Book Online</a></div> -->
+            <!-- <div class="book_button"><a href="booking.php">Book Online</a></div> -->
             <a href="tel:+79280106646">
                 <div class="header_phone d-flex flex-row align-items-center justify-content-center">
                     <img src="images/phone.png" alt="">
@@ -45,23 +45,23 @@
     <div class="menu_content">
         <nav class="menu_nav text-right">
             <ul>
-                <li><a href="index.html">Главная</a></li>
-                <li><a href="about.html">О нас</a></li>
+                <li><a href="index.php">Главная</a></li>
+                <li><a href="about.php">О нас</a></li>
                 <li><a href="#" class="mob-menu__show">Номера<span class="sp__arrow-d"></span></a>
                     <ul>
                         <li class="quicklinks-item">
-                            <a href="booking_2.html" class="main-menu__link">полулюкс 2-х местный</a>
+                            <a href="booking_2.php" class="main-menu__link">полулюкс 2-х местный</a>
                         </li>
                         <li class="quicklinks-item">
-                            <a href="booking_3.html" class="main-menu__link">полулюкс 3-х местный</a>
+                            <a href="booking_3.php" class="main-menu__link">полулюкс 3-х местный</a>
                         </li>
                         <li class="quicklinks-item">
-                            <a href="booking_4.html" class="main-menu__link">полулюкс 4-х местный</a>
+                            <a href="booking_4.php" class="main-menu__link">полулюкс 4-х местный</a>
                         </li>
                     </ul>
                 </li>
-                <li><a href="blog.html">Галерея</a></li>
-                <li><a href="contact.html">Контакты</a></li>
+                <li><a href="blog.php">Галерея</a></li>
+                <li><a href="contact.php">Контакты</a></li>
             </ul>
         </nav>
     </div>
